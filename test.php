0f40d41570e61bcc206334e239bbd2c27f044054
twagrobert <twagrobert2000@gmail.com>
2018-10-31 10:11:28 +0200

    Adding all files

---
+++ test.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>COSECSA 2018</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+    <!-- Bootstrap CSS File -->
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Libraries CSS Files -->
+    <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styless.css">
+    
+</head>
+<body>
+
+
+<!-- Add icon library -->
+
+<div class="container">
+    <div class="card_box">
+        <div class="row">
+            <div class="col-md-12 ">
+            <div class="row">
+                <?php
+                    for ($i=0; $i < 10; $i++) { ?>
+                       <div class="col-md-4 col-sm-6 col-sx-6">
+                    <div class="card">
+                        <img src="img/facts-bg.jpg" alt="" style="width:100%">
+                        <div class="container ">
+                            <div class="desc">
+                                <h1>Hotel Chez Lando</h1>
+
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, tempore sapiente eveniet
+                                    quibusdam ab ea, quaerat placeat numquam aspernatur, accusamus magnam neque.</p>
+
+
+
+                            </div>
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+               <?php     }
+
+                ?>
+                <!-- <div class="col-md-4 col-sm-6 col-sx-6">
+                    <div class="card">
+                        <img src="img/facts-bg.jpg" alt="" style="width:100%">
+                        <div class="container ">
+                            <div class="desc">
+                                <h1>Hotel Chez Lando</h1>
+
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, tempore sapiente eveniet
+                                    quibusdam ab ea, quaerat placeat numquam aspernatur, accusamus magnam neque.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div> -->
+                <!-- <div class="col-md-4 col-sm-6 col-sx-6">
+                    <div class="card">
+                        <img src="img/facts-bg.jpg" alt="" style="width:100%">
+                        <div class="container ">
+                            <div class="desc">
+                                <h1>Hotel Chez Lando</h1>
+
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, tempore sapiente eveniet
+                                    quibusdam ab ea, quaerat placeat numquam aspernatur, accusamus magnam neque.</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div> -->
+
+                <!-- <div class="col-md-4 col-sm-6 col-sx-6">
+                    <div class="card">
+                        <img src="img/facts-bg.jpg" alt="" style="width:100%">
+                        <div class="container ">
+                            <div class="desc">
+                                <h1>Hotel Chez Lando</h1>
+
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, tempore sapiente eveniet
+                                    quibusdam ab ea, quaerat placeat numquam aspernatur, accusamus magnam neque.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-sx-6">
+                    <div class="card">
+                        <img src="img/facts-bg.jpg" alt="" style="width:100%">
+                        <div class="container ">
+                            <div class="desc">
+                                <h1>Hotel Chez Lando</h1>
+
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, tempore sapiente eveniet
+                                    quibusdam ab ea, quaerat placeat numquam aspernatur, accusamus magnam neque.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div> -->
+                </div>
+
+            </div>
+        </div>
+
+
+        
+    </div>
+</div>
+
+
+</body>
+</html>
+
+
